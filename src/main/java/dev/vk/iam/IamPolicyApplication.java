ea99fdc25160e500673bc045d1689b01ed583477
Venkatakrishnan Ramesh <venkatkrishnan2003@gmail.com>
2026-08-30 04:22:58 +0530

    Add src/main/java/dev/vk/iam/IamPolicyApplication.java

---
+++ src/main/java/dev/vk/iam/IamPolicyApplication.java
@@ -0,0 +1,9 @@
+package dev.vk.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamPolicyApplication {
+  public static void main(String[] args) { SpringApplication.run(IamPolicyApplication.class, args); }
+}
